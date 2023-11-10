54df9c65fd49389b4cb4fb4def168f86d117f1df
Dorian-Grayy <150417850+Dorian-Grayy@users.noreply.github.com>
2023-11-10 13:41:59 +0700

    Add files via upload

---
+++ PtintBook/PtintBook/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace PtintBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btncountstr = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbstr = new System.Windows.Forms.TextBox();
+            this.dgw1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgw2 = new System.Windows.Forms.DataGridView();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbstr = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(209, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(264, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество страниц:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(122, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(556, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Количество листов, необходимое для печати: ";
+            // 
+            // btncountstr
+            // 
+            this.btncountstr.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btncountstr.Location = new System.Drawing.Point(183, 159);
+            this.btncountstr.Name = "btncountstr";
+            this.btncountstr.Size = new System.Drawing.Size(529, 40);
+            this.btncountstr.TabIndex = 2;
+            this.btncountstr.Text = "Рассчитать последовательность страниц";
+            this.btncountstr.UseVisualStyleBackColor = true;
+            this.btncountstr.Click += new System.EventHandler(this.btncountstr_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(436, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Первый проход (первая сторона) ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(500, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(419, 32);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Второй проход (вторая сторона)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(42, 303);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(389, 66);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Печать осуществляетс в два прохода\r\n(с двух сторон листа) с настройкой:\r\n2 страни" +
+    "цы на лист (порядок: нормальный).";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(543, 303);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(353, 66);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Необходимо перевернуть напечатанные \r\nстраницы относительно ороткого края. \r\nНаст" +
+    "ройки прежние.";
+            // 
+            // tbstr
+            // 
+            this.tbstr.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbstr.Location = new System.Drawing.Point(488, 8);
+            this.tbstr.Name = "tbstr";
+            this.tbstr.Size = new System.Drawing.Size(53, 39);
+            this.tbstr.TabIndex = 7;
+            // 
+            // dgw1
+            // 
+            this.dgw1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dgw1.Location = new System.Drawing.Point(29, 409);
+            this.dgw1.Name = "dgw1";
+            this.dgw1.RowHeadersWidth = 51;
+            this.dgw1.RowTemplate.Height = 24;
+            this.dgw1.Size = new System.Drawing.Size(402, 150);
+            this.dgw1.TabIndex = 8;
+            this.dgw1.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgw1_CellMouseDoubleClick1);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Тетрадь";
+            this.Column1.MinimumWidth = 150;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Первый проход";
+            this.Column2.MinimumWidth = 250;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 250;
+            // 
+            // dgw2
+            // 
+            this.dgw2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column4});
+            this.dgw2.Location = new System.Drawing.Point(527, 409);
+            this.dgw2.Name = "dgw2";
+            this.dgw2.RowHeadersWidth = 51;
+            this.dgw2.RowTemplate.Height = 24;
+            this.dgw2.Size = new System.Drawing.Size(392, 150);
+            this.dgw2.TabIndex = 9;
+            this.dgw2.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgw2_CellMouseDoubleClick);
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Тетрадь";
+            this.Column3.MinimumWidth = 150;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Второй проход";
+            this.Column4.MinimumWidth = 250;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 250;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(196, 579);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(593, 22);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Двойной клик по строке - скопировать последовательность страниц";
+            // 
+            // lbstr
+            // 
+            this.lbstr.AutoSize = true;
+            this.lbstr.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbstr.Location = new System.Drawing.Point(706, 86);
+            this.lbstr.Name = "lbstr";
+            this.lbstr.Size = new System.Drawing.Size(0, 33);
+            this.lbstr.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(949, 616);
+            this.Controls.Add(this.lbstr);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dgw2);
+            this.Controls.Add(this.dgw1);
+            this.Controls.Add(this.tbstr);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btncountstr);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Печать книг в Word";
+            ((System.ComponentModel.ISupportInitialize)(this.dgw1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btncountstr;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbstr;
+        private System.Windows.Forms.DataGridView dgw1;
+        private System.Windows.Forms.DataGridView dgw2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbstr;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ PtintBook/PtintBook/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PtintBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 1; i++)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    dgw1.Rows[i].Cells[j].Value = "Единая тетрадь";
+                    dgw2.Rows[i].Cells[j].Value = "Единая тетрадь";
+                }
+            }
+        }
+
+    private void btncountstr_Click(object sender, EventArgs e)
+        {
+            string count = tbstr.Text;
+            if (count == "")
+            {
+                MessageBox.Show("Введите количество страниц!");
+            }
+            else
+            {
+                double k = Convert.ToInt32(count);
+                double s = k / 4;
+                lbstr.Text = Math.Ceiling(s).ToString();
+
+                //первый проход
+                int nach = 1;
+                string strlen = count + "," + nach + ",";
+                //MessageBox.Show(strlen);
+                int cou = Convert.ToInt32(count);
+                //if (cou % 2 == 1) { strlen = strlen.Substring(0, strlen.Length - 2); }
+                while (cou >= 1 && nach >=1 && nach != cou && nach < cou-3)
+                {
+                        cou = cou - 2;
+                        nach += 2;
+                        strlen = strlen + cou + "," + nach + ",";
+                    //MessageBox.Show(strlen);
+                }
+                if (nach == cou) strlen = strlen.Substring(0, strlen.Length - 2);
+                MessageBox.Show(strlen);
+
+                //второй проход
+                int nach2 = 2;
+                int cou2 = Convert.ToInt32(count);
+                int cou22 = cou2 - 1;
+                string strlen2 = nach2 + "," + cou22 + ",";
+                //MessageBox.Show(strlen2);
+                while (cou22 >= 1 && nach2 >= 1 && nach2 != cou22 && nach2 < cou22-3)
+                {
+                    if (nach2 != cou22)
+                    {
+                        cou22 -= 2;
+                        nach2 += 2;
+                        strlen2 = strlen2 + nach2 + "," + cou22 + ",";
+                        //MessageBox.Show(strlen2);
+                    }
+                    else strlen2 = strlen2 + cou22 + ",";
+                }
+                if (nach2 == cou22) strlen2 = strlen2.Substring(0, strlen2.Length - 2);
+                MessageBox.Show(strlen2);
+
+                dgw1.Rows[0].Cells[1].Value = strlen;
+                dgw2.Rows[0].Cells[1].Value = strlen2;
+            }
+        }
+
+        private void dgw1_CellMouseDoubleClick1(object sender, DataGridViewCellMouseEventArgs e)
+        {
+                if(dgw1.Rows[0].Cells[1].Value != null)
+                Clipboard.SetText(dgw1[e.ColumnIndex, e.RowIndex].Value.ToString());
+                else MessageBox.Show("Рассчитайте последовательность страниц!"); 
+        }
+
+        private void dgw2_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (dgw2.Rows[0].Cells[1].Value != null)
+                Clipboard.SetText(dgw2[e.ColumnIndex, e.RowIndex].Value.ToString());
+            else MessageBox.Show("Рассчитайте последовательность страниц!");
+        }
+    }
+}
